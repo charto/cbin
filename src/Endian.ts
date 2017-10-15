8fe4eb98d9f755f61a059eb62197026f5e471d76
jjrv <juha@busfaster.com>
2017-10-15 08:16:22 +0300

    Fix endianness detection and bump version.

--- src/Endian.ts
+++ src/Endian.ts
@@ -8,17 +8,17 @@ export const enum CEndian {
 	little = 1
 }
 
-let native: CEndian;
+let nativeEndian: CEndian;
 const test = new Uint32Array(1);
 
 new Writer(new Uint8Array(test.buffer)).u32(0x01020304);
 
-if(test[0] == 0x01020304) native = CEndian.little;
-else if(test[0] == 0x04030201) native = CEndian.big;
+if(test[0] == 0x01020304) nativeEndian = CEndian.little;
+else if(test[0] == 0x04030201) nativeEndian = CEndian.big;
 else throw(new Error('Middle endian is not supported'));
 
 export enum Endian {
 	big = CEndian.big,
 	little = CEndian.little,
-	native = native
+	native = nativeEndian as number
 }
